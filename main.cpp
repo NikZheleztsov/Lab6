6de83d7870537906c08c8b11d8aae64e0a4afb39
Serpent <nikita.zheleztsov@mail.ru>
2020-11-04 21:56:32 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 //10 variant
 //Lab 6
-//Не получается решить проблему segfault при открытии файла. Работает в отдельных файлах, не работает здесь
 
 #include <algorithm>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
+#include <cstdlib>
+#include <iomanip>
 
 struct discipline
 {
@@ -49,7 +50,6 @@ std::ostream& operator << (std::ostream& out, std::vector<student>& stud)
       out << stud[i].subjects[k].name << ':' << stud[i].subjects[k].grade << ' ';
     out << '\n';
   }
-  out << '\0';
   return out;
 }
 
@@ -66,15 +66,19 @@ int main ()
   int num = 0, num2 = 0;
   std::vector<student> stud;
   std::vector<discipline> subj (num);
+  std::string file_str;
 
   if (answ == 'w')
   {
     file.open(name_file, std::ios::out);
     
+    if (file.is_open()) {
+    
     //Names of disciplines
     std::cout << "Please, enter number of subjects: ";
     std::cin >> num;
     subj.resize(num);
+    std::cout << "Please, enter names of subjects:\n";
     for (int i = 0; i < num; ++i)
       std::cin >> subj[i].name;
 
@@ -91,49 +95,101 @@ int main ()
       stud[i].subjects.resize (num);
       for (int k = 0; k < num; ++k)
       {
-        stud[i].subjects[k].name = subj[k].name; 
+        stud[i].subjects[k].name = subj[k].name;  //copying of subj
         std::cout << "Please, enter " << stud[i].FCs << "'s grade:\n";
         std::cout << stud[i].subjects[k].name << " : ";
         std::cin >> stud[i].subjects[k].grade;
       }
     }
+    } else std::cout << "Unable to open file\n";
   } else {
 
     file.open(name_file, std::ios::in);
-    std::string file_str;
+
+    if(file.is_open()) {
+
     std::getline(file, file_str,'\0');
 
 
     //Analazing of the file
     //Student's name
-    int pos = 0, last = 0, it = 0;
+    int pos = 0, last = 0, it = -1;
+    while (pos != -1)
+    {
+      pos = file_str.find('\n', pos + 1);
+      it++;
+    }
+    
+    stud.resize(it);
+    pos = 0, it = 0;
+
     while (pos != -1) 
     {
-      pos = file_str.find(" = ");
-      last = file_str.find('\n');
-      if (last != -1) 
-        stud[it].FCs = file_str.substr(last, pos - last);
-      else stud[it].FCs = file_str.substr(0, pos);
-      ++it;
+      pos = file_str.find(" = ", pos + 1);
+      last = file_str.rfind('\n', pos);
+      if (last != -1 && pos != -1 ) 
+         stud[it].FCs = file_str.substr(last + 1, pos - last - 1);
+      else if (pos != -1) stud[it].FCs = file_str.substr(0, pos);
+      it++;
     }
-    it = num2; 
-    last = 0;
+    num2 = it - 1;  //number of students
+    last = 0, it = 0, pos = 0;
 
     //Desciplines
     char k = 0;
     int i = 0, l = 0;
-    for (k = file_str[i]; k != '\n'; ++i)
+    last = file_str.find('\n', 0);
+    for (i = 0; i < last; i++)
     {
+      k = file_str[i];
       if (k == ':')
       {
-        last = file_str.rfind(' ');
-        subj[l].name = file_str.substr(last + 1, i - last - 1);
-        ++l;
+        it++;
       }
     }
-    l = num;
+    subj.resize(it);
+    num = it; //number of subj
+
+    for (i = 0; i < last; i++)
+    {
+      k = file_str[i];
+      if (k == ':')
+      {
+          pos = file_str.rfind(' ', i);
+          subj[l].name  = file_str.substr(pos + 1, i - pos - 1);
+          l++;
+      } 
+    }
+    
+    int new_pos = 0,
+        a = 0;
+
+    for (int l = 0; l < num2; ++l) //students
+    {
+      stud[l].subjects.resize(it);
+      for (int i = 0; i < it; i++) //subj
+          stud[l].subjects[i].name = subj[i].name;
+
+      for (int i = new_pos; i < last; i++)
+      {
+        k = file_str[i];
+        if (k == ':')
+        {
+          pos = file_str.find(' ', i);
+          stud[l].subjects[a].grade = std::stoi(file_str.substr(i+1, pos - i - 1));
+          a++;
+        }
+        if (a == 3) break;
+      }
+      new_pos = last + 1;
+      last = file_str.find('\n', last + 1);
+      a = 0;
+    }
+    } else std::cout << "Unable to open file\n";
   }
       
+  if (file.is_open()) {
+
   //Sorting
   std::sort(stud.begin(), stud.end(), compare); 
 
@@ -143,7 +199,8 @@ int main ()
   for (int i = 0; i < num2; ++i)
   {
     aver[i] = average(stud[i], num); 
-    std::cout << i+1 << ". " <<  stud[i].FCs << "\t Grade: " << aver[i] << '\n';
+    std::cout << i+1 << ". " <<  stud[i].FCs <<" | " <<  "Grade: " << aver[i] << std::endl;
+
   }
 
   //The best one
@@ -166,6 +223,29 @@ int main ()
   file << stud;
   file.close();
 
+  //Writing to binary
+  char new_answ = 0;
+  std::string new_name_file;
+  std::cout << "Do you want to write your database to a binary file? (y/n) ";
+  std::cin >> new_answ;
+  if (new_answ == 'y' )
+  {
+    std::cout << "Please, enter a name of a file: ";
+    std::cin >> new_name_file;
+    std::ofstream out (new_name_file, std::ios::binary);
+
+    if (answ == 'w')
+    {
+        file.open(name_file, std::ios::in);
+        std::getline(file, file_str,'\0');
+    }
+
+    out.write(reinterpret_cast<char*>(&file_str), file_str.size());
+    out.close(); 
+
   return 0;
+
+  }
+  }
 }
 
